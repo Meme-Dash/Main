d21ac28e1bd45a99a73f9249a90b13062bba27ea
21cj <36485376+21cj@users.noreply.github.com>
2018-03-01 12:02:43 -0600

    ry4
    65

--- Timer.py
+++ Timer.py
@@ -1,13 +1,10 @@
 from tkinter import *
 import time
-
 class timer:
     def timer(t):
         now = time.localtime(time.time())
         return now[5]
-
     seconds = 60
     current_sec = timer(60)
-
     if current_sec == 60:
         print (">>>>>>>>>>>>>>>>>>>>>"), secs
---
+++ timer4.py
@@ -0,0 +1,9 @@
+import time
+
+def countdown(t):
+    print('This window will remain open for 3 more seconds...')
+    while t >= 0:
+        print(t, end='...')
+        time.sleep(1)
+        t -= 1
+    print('Goodbye! \n \n \n \n \n')

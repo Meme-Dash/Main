db51dd1795bfc70322ccd8bd1a5a0feaf9e1f04f
Adogg34 <34487211+Adogg34@users.noreply.github.com>
2018-03-02 12:31:27 -0600

    Update MDtable.py

--- MDtable.py
+++ MDtable.py
@@ -3,14 +3,14 @@ from tkinter import *
 class Table:
     # initialize variables window
     def __init__(self, window, color="cyan",
-                 width=600, height=400):
+                 width=4475, height=1050):
         self.width = width
         self.height = height
         self.color = color
         self.canvas = Canvas(window, bg=self.color, height=self.height, width=self.width)
         self.canvas.pack()
-        font = ("monaco", 72)
-        self.scoreboard = self.canvas.create_text(300, 65, font=font, fill="red")
+        font = ("monaco", 150)
+        self.scoreboard = self.canvas.create_text(950, 525, font=font, fill="red")
     #remove item for colision with enemy sprite
     def remove_item(self, item):
          item.remove(self.canvas)
@@ -37,4 +37,3 @@ class Table:
     def draw_score(self, score):
         scores = str(score)
         self.canvas.itemconfigure(self.scoreboard, text=scores)
-
